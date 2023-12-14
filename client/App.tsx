aeb0d20d2bb78b6528a402a30f98989d9dc38e27
s1f10210254 <s1f102102548@iniad.org>
2023-12-15 03:49:32 +0900

    setup

--- client/App.tsx
+++ client/App.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { fetchGreeting } from './src/api';  // APIリクエスト関数をインポート
+import { fetchGreeting } from './src/api/api';  // APIリクエスト関数をインポート
 
 export default function App() {
   const [apiData, setApiData] = useState(null);
@@ -13,9 +13,11 @@ export default function App() {
       .catch(error => console.error('API error:', error));
   }, []);
 
+  console.log(apiData)
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+      
       {apiData && (
         <Text>Data from API: {JSON.stringify(apiData)}</Text>
       )}
