aff773bfd8315a5b76965149a00d48cea7eb027a
Hiroki <hiroki@HirokinoMacBook-Pro.local>
2023-12-14 20:24:47 +0900

    動かせた7

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [test, setTest] = useState(true)
+  console.log(test)
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app kodgksaogks</Text>
+      <Text>Open up App.tsx to start working on your app kodgksaogksgggg</Text>
       <StatusBar style="auto" />
+      {test &&(
+        
+            <Text>aaaaa</Text>
+        
+        
+      )}
     </View>
   );
 }
