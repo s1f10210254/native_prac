eca3d3d6ae2138497313f7eabf5d2ccff1c187da
s1f10210254 <s1f102102548@iniad.org>
2023-12-16 20:02:39 +0900

    add prisma

--- server/api/app.ts
+++ server/api/app.ts
@@ -1,23 +1,28 @@
 //server/api/api.ts
 
+import { PrismaClient } from '@prisma/client';
 import { FastifyReply, FastifyRequest } from 'fastify';
-
-// import fastify from '../node_modules/fastify/fastify';
-// const server = fastify({
-//   logger: true,
-// });
-const fastify = require('fastify')({ logger: true });
-
+const server = require('fastify')({ logger: true });
+const prisma = new PrismaClient();
 // Declare a route
-fastify.get('/', (request: FastifyRequest, reply: FastifyReply) => {
+server.get('/', (request: FastifyRequest, reply: FastifyReply) => {
   reply.send({ hello: 'world' });
 });
 
+server.get('/api/greet', (request: FastifyRequest, reply: FastifyReply) => {
+  reply.type('application/json').code(200);
+  return { hello: 'world' };
+});
+
+server.get('/api/users', (request: FastifyRequest, reply: FastifyReply) => {
+  const userIds = prisma.user.findMany();
+  return { userIds };
+});
 const start = async () => {
   try {
-    await fastify.listen(3000);
+    await server.listen(3000);
   } catch (err) {
-    fastify.console.log.console.error(err);
+    server.console.log.console.error(err);
     process.exit(1);
   }
 };
